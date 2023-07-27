f5d2697e04a5254967f1931463cfda08a8861ce6
root <root@emacs.emacs>
2023-07-28 00:03:20 +0800

    fix flow

--- custom_components/volvooncall_cn/config_flow.py
+++ custom_components/volvooncall_cn/config_flow.py
@@ -10,7 +10,10 @@ class VolvoOnCallCnConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, info):
         if info is not None:
-            pass  # TODO: process info
+            await self.async_set_unique_id(user_input["username"])
+            return self.async_create_entry(
+                title=user_input[username], data=user_input
+            )
 
         return self.async_show_form(
             step_id="user", data_schema=vol.Schema(
