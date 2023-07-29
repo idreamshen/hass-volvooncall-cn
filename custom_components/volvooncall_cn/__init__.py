40e5c192350fcc3cba2b181ab0237b5e09ae0230
root <root@emacs.emacs>
2023-07-29 08:34:22 +0800

    change device name

--- custom_components/volvooncall_cn/__init__.py
+++ custom_components/volvooncall_cn/__init__.py
@@ -208,7 +208,7 @@ class VolvoEntity(CoordinatorEntity):
         """Return a inique set of attributes for each vehicle."""
         return DeviceInfo(
             identifiers={(DOMAIN, self.coordinator.data[self.idx].vin)},
-            name=self.coordinator.data[self.idx].series_name,
+            name="Volvo " + self.coordinator.data[self.idx].series_name,
             model=self.coordinator.data[self.idx].model_name,
             manufacturer="Volvo",
         )
